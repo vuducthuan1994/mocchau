ed83a1705bde5b0cf96cbe0ebab980c9906905bf
Vu Duc Thuan <vuducthuan1994@gmail.com>
2020-02-20 01:34:58 +0700

    add animation and fix bug

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
     $('#owl-carousel-1').owlCarousel({
         lazyLoad: false,
         autoplay: false,
@@ -10,8 +11,6 @@ $(document).ready(function() {
         margin: 40,
         slideSpeed: 300,
         paginationSpeed: 400
-            // navText: [$('.am-next'), $('.am-prev')],
-            // navContainer: '#customNav'
     });
     $('#owl-carousel-2').owlCarousel({
         lazyLoad: false,
@@ -25,41 +24,78 @@ $(document).ready(function() {
         slideSpeed: 300,
         paginationSpeed: 400,
     });
-    AOS.init({
-        once: false,
-        mirror: true,
-        delay: 500,
-        easing: 'ease-out-back',
-        duration: 2200
-    })
+
     $('#scroll_to_top').click(function(event) {
         event.preventDefault();
         $('html, body').animate({ scrollTop: 0 }, 700);
         return false;
     });
+
+
+    newsSliderController();
+    textSliderController();
+    addAnimationWhenScroll();
+    preLoading();
+});
+
+function preLoading() {
+    $(".img-header").on('load', function() {
+        $(".pre-loading").hide();
+        setAnimationForSloganWhenFirstScroll();
+        initAnimationForAllSection();
+    });
+}
+
+function initAnimationForAllSection() {
+    AOS.init({
+        once: false,
+        mirror: true,
+        delay: 400,
+        easing: 'ease-out-back',
+        duration: 2000
+    });
+}
+
+function setAnimationForSloganWhenFirstScroll() {
+    $(window).one("scroll", function() {
+        $(".test").css("transform", "scaleY(1)");
+    });
+}
+
+function addAnimationWhenScroll() {
+    var image = document.getElementsByClassName('img-header');
+    new simpleParallax(image, {
+        delay: 2,
+        scale: 1.2
+    });
+}
+
+function newsSliderController() {
     var owl1 = $('#owl-carousel-1');
     // Go to the next item of slider news
     $('.am-next').click(function() {
         owl1.trigger('next.owl.carousel', [300]);
-    })
+    });
 
     // Go to the previous news
     $('.am-prev').click(function() {
         // With optional speed parameter
         // Parameters has to be in square bracket '[]'
         owl1.trigger('prev.owl.carousel', [300]);
-    })
+    });
+}
 
+function textSliderController() {
     var owl2 = $('#owl-carousel-2');
     // Go to the next item of slider text
     $('.btn-pre-next-slider').click(function() {
         owl2.trigger('next.owl.carousel', [300]);
-    })
+    });
 
-    // Go to the previous item
+    // Go to the previous item of sliderText
     $('.btn-pre-slider').click(function() {
         // With optional speed parameter
         // Parameters has to be in square bracket '[]'
         owl2.trigger('prev.owl.carousel', [300]);
-    })
-});
+    });
+}
